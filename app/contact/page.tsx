beed4f140ad1000cc4122b2d9b8e9bd8dc773330
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2023-04-21 10:51:44 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3230.6840452415117!2d0.08745471028791241!3d35.93023297239328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128202139598f319%3A0xc5767572247a435d!2sPolyglo!5e0!3m2!1sen!2sdz!4v1682027348716!5m2!1sen!2sdz"
     style={{ border: 0 }}
-    allowFullScreen=""
+<!--     allowFullScreen="" -->
     loading="lazy"
     referrerPolicy="no-referrer-when-downgrade"
     className="absolute inset-0 w-full h-full"
