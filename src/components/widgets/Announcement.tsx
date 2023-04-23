9b471cd41dfe54774e535a2a006f0c679941857a
Chekkal Youcef <chekkalislam@gmail.com>
2023-04-23 23:54:00 +0100

    SEO fixing

--- src/components/widgets/Announcement.tsx
+++ src/components/widgets/Announcement.tsx
@@ -4,13 +4,13 @@ import { AnnouncementProps } from '~/shared/types';
 const Announcement = ({ title, callToAction, callToAction2 }: AnnouncementProps) => {
 
   return (
-    <div className="hidden overflow-hidden text-ellipsis whitespace-nowrap border-b border-blue-900 bg-blue-900 px-3 py-2 text-sm text-gray-200 md:block">
-      <span className="bg-blue-800 py-0.5 px-1 text-xs font-semibold">{title}</span>{' '}
+    <div className="hidden overflow-hidden text-ellipsis whitespace-nowrap border-b  border-blue-900 bg-blue-900 px-3 py-2 text-sm text-gray-200 md:block">
+      <span className="bg-blue-800 py-0.5 px-1 text-xs font-semibold ">{title}</span>{' '}
       {callToAction && callToAction.text && callToAction.href && (
         <a
-          // href={callToAction.href}
+          href={callToAction.href}
           // target="_blank"
-          // rel="noreferrer noopened"
+          rel="noreferrer noopened"
           className="cursor-pointer text-gray-100 hover:underline"
         >
           {callToAction.icon && <callToAction.icon className="mr-1 -ml-1.5 h-5 w-5" />} {callToAction.text}
