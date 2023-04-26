9677b71fb4008de9f35a88b4f3286d1f8e95522b
Chekkal Youcef <chekkalislam@gmail.com>
2023-04-26 22:55:12 +0100

    added vercel analytics

--- src/components/atoms/Providers.tsx
+++ src/components/atoms/Providers.tsx
@@ -1,14 +1,16 @@
-'use client';
+"use client";
 
-import { ThemeProvider } from 'next-themes';
-
+import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 export interface ProvidersProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => (
   <ThemeProvider attribute="class" disableTransitionOnChange>
     {children}
+
+    <Analytics />
   </ThemeProvider>
 );
 
